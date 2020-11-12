fb2681b7a089a508af96b058e110c9b1af544530
MD KAIMUL <kaimuljewel@yahoo.com>
2020-11-13 00:43:55 +1100

    first commit

---
+++ app.js
@@ -0,0 +1,6 @@
+const bar = document.querySelector('.navbar');
+const navbar = document.querySelector('.navbar');
+
+bar.addEventListener("click", function() {
+   navbar.classList.toggle('change')
+})
